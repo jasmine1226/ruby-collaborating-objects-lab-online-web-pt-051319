8e99ce0a6fbdaaec79865d01ceba651c98e655e8
Jasmine Chen <jasmine1226@gmail.com>
2019-06-11 07:32:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     song = self.new(filename[0])
     song.genre = filename[2].chomp('.mp3')
     @artist = Artist.find_or_create_by_name(filename[1])
-    
+
     @artist.add_song(song)
   end
 end
