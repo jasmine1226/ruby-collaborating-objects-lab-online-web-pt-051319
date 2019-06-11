e9ad45c99ebd69159a28701cb848287673a5073e
Jasmine Chen <jasmine1226@gmail.com>
2019-06-11 05:49:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, @genre
 
   def initialize(name)
     @name = name
@@ -7,6 +7,7 @@ class Song
 
   def new_by_filename(filename)
     @artist = file.name.split(" - ")[0]
-
+    @name = file.name.split(" - ")[1]
+    @genre = file.name.split(" - ")[2]
   end
 end
