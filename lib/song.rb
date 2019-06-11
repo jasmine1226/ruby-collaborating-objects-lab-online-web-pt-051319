1d7c7861be1fdc68549ac9714bbc7d966bee4592
Jasmine Chen <jasmine1226@gmail.com>
2019-06-11 07:46:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,6 @@ class Song
 
   def self.new_by_filename(filename)
     filename = filename.split(" - ")
-    puts "artist = #{filename[0]}"
-    puts "name = #{filename[1]}"
     song = self.new(filename[1])
     song.genre = filename[2].chomp('.mp3')
     @artist = Artist.find_or_create_by_name(filename[0])
