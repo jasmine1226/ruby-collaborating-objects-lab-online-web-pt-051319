6a15ba38bbc6defe10d56ed0d21d57f2b1138afe
Jasmine Chen <jasmine1226@gmail.com>
2019-06-11 05:56:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @name = name
   end
 
-  def new_by_filename(filename)
+  def self.new_by_filename(filename)
     @artist = file.name.split(" - ")[0]
     @name = file.name.split(" - ")[1]
     @genre = file.name.split(" - ")[2].chomp('.mp3')
